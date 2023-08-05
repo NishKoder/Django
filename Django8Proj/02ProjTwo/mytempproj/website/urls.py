ab667330799de098efde26d44a65139669e190cc
NishKoder <shub002gupta@gmail.com>
2023-08-05 20:06:11 +0530

    Complete Proj 2

--- Django8Proj/02ProjTwo/mytempproj/website/urls.py
+++ Django8Proj/02ProjTwo/mytempproj/website/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomePageView.as_view(), name="Home"),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
+    path("contactus/", views.ContactUsPageView.as_view(), name="contactus"),
 ]
--- Django8Proj/02ProjTwo/mytempproj/website/views.py
+++ Django8Proj/02ProjTwo/mytempproj/website/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "homepage.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ContactUsPageView(TemplateView):
+    template_name = "contactus.html"
