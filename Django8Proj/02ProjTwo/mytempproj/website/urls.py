51fd613ea044c76a6a1e23d7a9f3658e243c1168
NishKoder <shub002gupta@gmail.com>
2023-08-05 16:55:11 +0530

    Proj1-2

--- Django8Proj/02ProjTwo/mytempproj/website/urls.py
+++ Django8Proj/02ProjTwo/mytempproj/website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for helloworld project.
+URL configuration for mytempproj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", include('hola.urls')),
+    path("", views.HomePageView.as_view(), name="Home"),
 ]
